4d82fd824f40304170bb1b1eb982e843a98015ff
Senates <coopsnoopftw@gmail.com>
2021-11-29 09:23:30 +1300

    commit

---
+++ CryptoTracker/CryptoTracker.Android/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace CryptoTracker.Droid
+{
+    [Activity(Label = "CryptoTracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            AppCenter.Start("android=2bdc5ac3-b1b3-4342-b676-668eaaccde86;" +
+                              "uwp={Your UWP App secret here};" +
+                              "ios={Your iOS App secret here}",
+                              typeof(Analytics), typeof(Crashes));
+            
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+            //Crashes.GenerateTestCrash();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ CryptoTracker/CryptoTracker/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using CoinAPI;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft;
+using CryptoTracker.Model;
+
+namespace CryptoTracker
+{
+    public partial class MainPage : ContentPage
+    {
+        private string apikey = "81051166-91A3-413E-8E7F-CF78203F69A9";
+        private string baseimageurl = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_32/";
+        public MainPage()
+        {
+            InitializeComponent();
+            coinListView.ItemsSource = GetCoin();
+        }
+        private void Refresh_Clicked(object sender, EventArgs e)
+        {
+            coinListView.ItemsSource = GetCoin();
+        }
+        private List<Coin> GetCoin()
+        {
+            List<Coin> coins;
+            var client = new RestClient("http://rest.coinapi.io/v1/assets?filter_asset_id=ETH;XRP;BTC;LTC;DOGE");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("X-CoinAPI-Key", apikey);
+            var response = client.Execute(request);
+            coins = JsonConvert.DeserializeObject<List<Coin>>(response.Content);
+            foreach (var c in coins)
+            {
+                c.icon_url = baseimageurl + c.id_icon.Replace("-", "") + ".png";
+            }
+            return coins;
+        }
+    }
+}
